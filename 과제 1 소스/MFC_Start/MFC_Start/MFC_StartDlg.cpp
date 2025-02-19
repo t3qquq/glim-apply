c59b94b3a0bca120839346e4442747b160073819
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-02-19 22:47:17 +0900

    과제 1

---
+++ 과제 1 소스/MFC_Start/MFC_Start/MFC_StartDlg.cpp
@@ -0,0 +1,44 @@
+﻿﻿
+#include "pch.h"
+#include "framework.h"
+#include "MFC_Start.h"
+#include "MFC_StartDlg.h"
+#include "afxdialogex.h"
+#include "resource.h"  // resource.h 포함 추가
+#include <random>
+#include <gdiplus.h>
+
+using namespace Gdiplus;
+#pragma comment(lib, "gdiplus.lib")
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CMFCStartDlg::CMFCStartDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFC_START_DIALOG, pParent), m_Radius(10), m_Thickness(2), m_IsDragging(false), m_IsRandomMoving(false)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+CMFCStartDlg::~CMFCStartDlg()
+{
+	if (m_RandomMoveThread.joinable()) {
+		m_IsRandomMoving = false;
+		m_RandomMoveThread.join();
+	}
+}
+
+void CMFCStartDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCStartDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONUP()
+	ON_BN_CLICKED(IDC_BUTTON_RESET, &CMFCStartDlg::OnBnClickedReset)
+	ON_BN_CLICKED(IDC_BUTTON_RANDOM_MOVE, &CMFCStartDlg::OnBnClickedRandomMove)
+END_MESSAGE_MAP()
---
+++ 과제 1 소스/MFC_Start/MFC_Start/MFC_StartDlg.h
@@ -0,0 +1,48 @@
+﻿﻿﻿
+#pragma once
+#include <vector>
+#include <thread>
+#include <afxwin.h>
+
+// CMFCStartDlg 대화 상자
+class CMFCStartDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCStartDlg(CWnd* pParent = nullptr);
+	~CMFCStartDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_START_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+	std::vector<CPoint> m_ClickPoints;
+	int m_Radius;      // 클릭 지점 원 반지름
+	int m_Thickness;   // 정원 테두리 두께 (누락된 변수 추가)
+	bool m_IsDragging;
+	int m_DragIndex;
+	std::thread m_RandomMoveThread;
+	bool m_IsRandomMoving;
+
+	void DrawCircle(CDC* pDC);
+	void MoveRandomPoints();
+
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnBnClickedReset();
+	afx_msg void OnBnClickedRandomMove();
+
+	DECLARE_MESSAGE_MAP()
+};
